b60682e82d65cf8312ff759d251c81a9116fc906
DilumVihanga <vihangadilum6@gmail.com>
2023-07-01 19:24:05 +0530

    fixed application was crashin on first load and modified category items

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,12 +6,12 @@ import {mobile} from '../Responsive'
 
 const Container = styled.div`
     display: flex;
+    flex-wrap: wrap;
     padding: 20px;
-    justify-content: space-between;
+    gap: 10px;
+    justify-content: center;
     align-items: center;
-    object-fit: cover;
-    object-position: center;
-    overflow: hidden;
+    
     
     
     ${mobile({
