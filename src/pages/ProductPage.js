ef041ca2470caa509e95c0d58d8e58549747dfec
DilumVihanga <vihangadilum6@gmail.com>
2023-07-01 15:32:46 +0530

    aded cart payment and connected with backend

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -23,7 +23,6 @@ const Wrapper = styled.div`
     
     ${mobile({
         flexDirection: "column",
-        padding: "20px 10px"
     })}
 
 `
@@ -57,10 +56,7 @@ const Image = styled.img`
 const InfoContainer = styled.div`
     padding: 0px 20px;
     flex: 1;
-
-    ${mobile({
-        padding: "0px 0px",
-    })}
+    
     
 `
 
@@ -208,10 +204,8 @@ const Button = styled.button`
 function ProductPage(props) {
     const [product, setProduct] = useState({})
     const [ProductQuentity, setProductQuentity] = useState(1)
-
-    //setting defalut size and color for product
-    const [Color, setColor] = useState((product?.color?.length >= 0 && `#${product.color[0]}`) || "#000000");
-    const [size, setsize] = useState((product?.size?.length >= 0 && product.size[0]) || "XL");
+    const [Color, setColor] = useState();
+    const [size, setsize] = useState();
     
     //to change title as soon as component mounts
     useEffect(() => {
@@ -272,20 +266,10 @@ function ProductPage(props) {
         } 
 
 
-        const {data:{order}} = await userRequest.post("api/buy/checkout",{
-            user:user._id,
-            product: {
-                productID: product._id,
-                quantity:ProductQuentity,
-                size, 
-                color:Color,
-            },
-            type: "product"
-        });
-
+        const {data:{order}} = await userRequest.post("api/buy/checkout",{productID: product._id, quantity:ProductQuentity, size, color:Color,user:user._id});
         const {data:{key}} = await userRequest.get("api/buy/getkey");
 
-        if(!order || !key){
+        if(!order || key){
             return console.log("error accured while creating order");
             //TODO: add Message Prompt
         }
